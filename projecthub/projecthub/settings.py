395f0da0e8a8cfde323d78bc8da0e7d38b0a21fc
yoramclapper <yoram.t.c@gmail.com>
2025-04-04 06:23:08 +0200

    Updated settings with timezone and language

--- projecthub/projecthub/settings.py
+++ projecthub/projecthub/settings.py
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'nl-NL'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Amsterdam'
 
 USE_I18N = True
 

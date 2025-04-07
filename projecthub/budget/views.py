8aba6cdc1f11b5e44586f56acb93710bbf6ab5db
yoramclapper <yoram.t.c@gmail.com>
2025-04-07 20:49:20 +0200

    Created first functional version of budget-form (without error-handling yet)

--- projecthub/budget/views.py
+++ projecthub/budget/views.py
@@ -6,7 +6,7 @@ def budget_view(request):
     if request.method == 'POST':
         form = BudgetForm(request.POST)
         if form.is_valid():
-            pass  # does nothing, just trigger the validation
+            form.save()
     else:
         form = BudgetForm()
     return render(request, 'budget/budget.html', {'form': form})

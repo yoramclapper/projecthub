5d8ad7e2666681b5a31e4de867660ff0da83444d
yoramclapper <yoram.t.c@gmail.com>
2025-04-08 11:28:55 +0200

    Restructured inheritance of html-components

--- projecthub/budget/urls.py
+++ projecthub/budget/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'budget'
+
 urlpatterns = [
     path("", views.budget_view, name="budget"),
 ]
--- projecthub/projecthub/settings.py
+++ projecthub/projecthub/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -53,10 +54,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'projecthub.urls'
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'core', 'templates', 'core')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
